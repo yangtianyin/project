84841c498c1911cd8a24e53627854bb70fa87ef2
yangtianyin <359862119@qq.com>
2018-12-28 17:34:25 +0800

    测试

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 // 数据库配置
 $config['db']['host'] = 'localhost';
 $config['db']['username'] = 'root';
-$config['db']['password'] = '123456';
+$config['db']['password'] = 'root';
 $config['db']['dbname'] = 'project';
 
 // 默认控制器和操作名
